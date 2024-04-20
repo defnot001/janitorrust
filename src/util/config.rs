1af93e421f8fd5a86359ddb35a2a079ebffbe36a
defnot001 <marc.miller994@gmail.com>
2024-04-20 18:58:53 +0200

    deserialize ids directly

--- src/util/config.rs
+++ src/util/config.rs
@@ -1,14 +1,15 @@
 use anyhow::Context;
 use serde::Deserialize;
+use serenity::all::{ChannelId, GuildId, UserId};
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct Config {
     pub bot_token: String,
     pub database_url: String,
-    pub superuser: String,
-    pub admins_server_id: String,
-    pub admin_server_log_channel: String,
-    pub admin_server_error_log_channel: String,
+    pub superuser: UserId,
+    pub admins_server_id: GuildId,
+    pub admin_server_log_channel: ChannelId,
+    pub admin_server_error_log_channel: ChannelId,
 }
 
 impl Config {
