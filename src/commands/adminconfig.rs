22618b27acc26e6f29c01a231a6ee269a838cf2d
defnot001 <marc.miller994@gmail.com>
2024-04-20 21:19:04 +0200

    Finish adminconfig command

--- src/commands/adminconfig.rs
+++ src/commands/adminconfig.rs
@@ -1,6 +1,20 @@
+use std::num::NonZeroU64;
+
+use poise::CreateReply;
 use serenity::all::GuildId;
 
-use crate::{database::serverconfig_model_controller::ServerConfigComplete, Context};
+use crate::{
+    assert_admin, assert_admin_server,
+    database::{
+        badactor_model_controller::BadActorModelController,
+        serverconfig_model_controller::{ServerConfigComplete, ServerConfigModelController},
+    },
+    util::{
+        error::{respond_error, respond_mistake},
+        screenshot::FileManager,
+    },
+    Context,
+};
 
 /// Subcommands for admins to inspect the bot's server configs.
 #[poise::command(
@@ -13,124 +27,107 @@ pub async fn adminconfig(_: Context<'_>) -> anyhow::Result<()> {
     Ok(())
 }
 
-/// Delete a bad actor from the database
+/// Display the configs for up to 5 servers at a time.
 #[poise::command(slash_command)]
 async fn display_configs(
     ctx: Context<'_>,
     #[description = "The ID(s) of the server(s) to display the config for. Separate multiple IDs with a comma (,). Max 5."]
     guild_id: String,
 ) -> anyhow::Result<()> {
+    assert_admin_server!(&ctx);
+    assert_admin!(&ctx);
     ctx.defer().await?;
 
-    let guild_ids = guild_id
-        .split(',')
-        .map(|id| id.trim().parse::<u64>())
-        .collect::<Result<Vec<u64>, _>>();
+    let guild_ids = match parse_guild_ids(&guild_id) {
+        Ok(ids) => {
+            if ids.is_empty() || ids.len() > 5 {
+                let msg = format!("Expected between 1 and 5 guilds, got {}", ids.len());
+                return respond_mistake(msg, &ctx).await;
+            }
 
-    if guild_ids.is_err() {
-        ctx.say("Invalid guild ID(s) provided!").await?;
-        return Ok(());
-    }
-
-    let guild_ids = guild_ids.unwrap();
-
-    if guild_ids.len() > 5 {
-        ctx.say("You can only display the  config for up to 5 servers at a time!")
-            .await?;
-        return Ok(());
-    }
-
-    let mut guilds = Vec::new();
-
-    for guild_id in guild_ids {
-        if guild_id == 0 {
-            ctx.say("Invalid guild ID(s) provided!").await?;
-            return Ok(());
+            ids
         }
-
-        let partial_guild = GuildId::new(guild_id).to_partial_guild(&ctx).await?;
-
-        guilds.push(partial_guild);
-    }
-
-    let mut response = String::new();
-
-    for guild in guilds {
-        response.push_str(&format!("Server: {}\nConfig: {}\n", guild.name, "config"));
-    }
-
-    ctx.say(response).await?;
-    Ok(())
-}
-
-/// Another subcommand of `parent`
-#[poise::command(slash_command)]
-async fn delete_bad_actor(ctx: Context<'_>) -> anyhow::Result<()> {
-    ctx.defer().await?;
-    ctx.say("You invoked the second child command!").await?;
-    Ok(())
-}
-
-async fn build_server_config_embed(
-    config: ServerConfigComplete,
-    ctx: &Context<'_>,
-) -> anyhow::Result<CreateEmbed> {
-    let Ok(guild) = config.server_config.server_id.to_partial_guild(&ctx).await else {
-        return Err(anyhow::anyhow!(
-            "Failed to retrieve guild with id {} from the API!",
-            config.server_config.server_id,
-        ));
+        Err(e) => {
+            let msg = "One or more of the guilds ids you provided are invalid";
+            return respond_mistake(msg, &ctx).await;
+        }
     };
 
-    let Ok(config_users) = UserModelController::get_by_guild(&ctx.data().db_pool, &guild.id).await
-    else {
-        return Err(anyhow::anyhow!(
-            "Failed to retrieve users for {} from the database!",
-            display(&guild),
-        ));
-    };
+    let target_amount = guild_ids.len();
 
-    let user_ids = config_users.iter().map(|user| user.id).collect::<Vec<_>>();
-
-    let Ok(discord_users) = get_users(user_ids, &ctx).await else {
-        return Err(anyhow::anyhow!(
-            "Failed to retrieve users for {} from the API!",
-            display(&guild),
-        ));
-    };
-
-    let log_channel = if let Some(channel_id) = config.server_config.log_channel {
-        let Ok(channel) = channel_id.to_channel(&ctx).await else {
-            return Err(anyhow::anyhow!(
-                "Failed to retrieve log channel with id {} from the API!",
-                channel_id,
-            ));
+    let configs =
+        match ServerConfigModelController::get_multiple_by_guild_id(&ctx.data().db_pool, guild_ids)
+            .await
+        {
+            Ok(configs) => configs,
+            Err(e) => {
+                let msg = "Failed to get the config for one or more servers from the database";
+                return respond_error(msg, e, &ctx).await;
+            }
         };
 
-        Some(channel)
-    } else {
-        None
+    let mut embeds = Vec::with_capacity(target_amount);
+
+    for config in configs {
+        let full_config =
+            ServerConfigComplete::from_server_config(config, &ctx.data().db_pool, &ctx).await?;
+
+        embeds.push(full_config.to_embed(ctx.author()))
+    }
+
+    ctx.send(CreateReply {
+        embeds,
+        ..Default::default()
+    })
+    .await?;
+
+    Ok(())
+}
+
+/// Delete a bad actor from the database.
+#[poise::command(slash_command)]
+async fn delete_bad_actor(
+    ctx: Context<'_>,
+    #[description = "The entry id that you want to delete."] entry: u64,
+) -> anyhow::Result<()> {
+    assert_admin_server!(&ctx);
+    assert_admin!(&ctx);
+    ctx.defer().await?;
+
+    let deleted = match BadActorModelController::delete(&ctx.data().db_pool, entry).await {
+        Ok(deleted) => deleted,
+        Err(e) => {
+            let msg = format!("Failed to delete entry with id {entry} from the database");
+            return respond_error(msg, e, &ctx).await;
+        }
     };
 
-    let mut embed = create_default_embed(ctx)
-        .title(format!("Server Config for {}", &guild.name))
-        .field("Server ID", inline_code(&guild.id.to_string()), false)
-        .field(
-            "Whitelisted Admins",
-            discord_users
-                .into_iter()
-                .map(|user| fdisplay(&user))
-                .collect::<Vec<_>>()
-                .join("\n"),
-            false,
-        )
-        .field(
-            "Log Channel",
-            log_channel
-                .map(|channel| fdisplay(&channel))
-                .unwrap_or_else(|| "Not set".to_string()),
-            false,
-        );
+    tracing::info!("Deleted bad actor entry with id {entry} from the database.");
 
-    Ok(embed)
+    if let Some(file_name) = deleted.screenshot_proof.as_ref() {
+        if let Err(e) = FileManager::delete(file_name).await {
+            let msg = format!("Bad actor with id {entry} was successfully deleted from the database but deleting the screenshot failed. Please do so manually");
+            return respond_error(msg, e, &ctx).await;
+        }
+
+        tracing::info!("Deleted screenshot {file_name} from the file system.");
+    }
+
+    ctx.say(format!("Successfully deleted bad actor entry with id {entry} from the database. If they had a screenshot, it was also deleted.")).await?;
+    Ok(())
+}
+
+fn parse_guild_ids(str: &str) -> anyhow::Result<Vec<GuildId>> {
+    str.split(',')
+        .map(|id| match id.parse::<u64>() {
+            Ok(id) => {
+                if let Some(non_zero) = NonZeroU64::new(id) {
+                    Ok(GuildId::from(non_zero))
+                } else {
+                    anyhow::bail!("0 is not a valid guild id")
+                }
+            }
+            Err(e) => anyhow::bail!(e),
+        })
+        .collect()
 }
