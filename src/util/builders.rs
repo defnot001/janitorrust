f171fdf98181100bea2d0fea7a334afcf7c925fc
defnot001 <marc.miller994@gmail.com>
2024-04-22 20:19:08 +0200

    add embed colors

--- src/util/builders.rs
+++ src/util/builders.rs
@@ -1,4 +1,4 @@
-use serenity::all::{CreateEmbed, CreateEmbedFooter, User};
+use serenity::all::{Colour, CreateEmbed, CreateEmbedFooter, User};
 
 pub fn create_default_embed(interaction_user: &User) -> CreateEmbed {
     let name = if let Some(global) = interaction_user.global_name.clone() {
@@ -17,4 +17,31 @@ pub fn create_default_embed(interaction_user: &User) -> CreateEmbed {
         .color(3_517_048)
         .footer(footer)
         .timestamp(chrono::Utc::now())
+}
+
+#[derive(Default, poise::ChoiceParameter)]
+pub enum EmbedColor {
+    #[default]
+    Kiwi = 0x35AA78,
+    Black = 0x000000,
+    Gray = 0xBEBEBE,
+    White = 0xFFFFFF,
+    Blue = 0x0000FF,
+    Cyan = 0x00FFFF,
+    Green = 0x00FF00,
+    Orange = 0xFFA500,
+    Coral = 0xFF7F50,
+    Red = 0xFF0000,
+    DeepPink = 0xFF1493,
+    Purple = 0xA020F0,
+    Magenta = 0xFF00FF,
+    Yellow = 0xFFFF00,
+    Gold = 0xFFD700,
+    None = 0x2F3136,
+}
+
+impl From<EmbedColor> for Colour {
+    fn from(colour: EmbedColor) -> Self {
+        Colour(colour as u32)
+    }
 }
