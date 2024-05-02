ea124796c81503933f8b1b49e3f9f30f4de5ff3b
defnot001 <marc.miller994@gmail.com>
2024-05-02 21:14:14 +0200

    add more random functions

--- src/util/random_utils.rs
+++ src/util/random_utils.rs
@@ -1,18 +1,35 @@
 use std::{num::NonZeroU64, str::FromStr};
 
 use anyhow::Context;
-use futures::future::try_join_all;
-use serenity::all::{CacheHttp, CreateEmbed, GuildId, PartialGuild, RoleId, User, UserId};
+use futures::future;
+use poise::serenity_prelude as serenity;
+use serenity::{CacheHttp, CreateEmbed, GuildId, PartialGuild, RoleId, User, UserId};
+
+use crate::Context as AppContext;
 
 pub fn parse_snowflake(snowflake: impl Into<String>) -> anyhow::Result<std::num::NonZeroU64> {
     NonZeroU64::from_str(snowflake.into().as_str()).map_err(anyhow::Error::new)
+}
+
+pub async fn get_bot_user(ctx: AppContext<'_>) -> Option<User> {
+    ctx.framework()
+        .bot_id
+        .to_user(ctx)
+        .await
+        .context("Failed to get bot user")
+        .ok()
+}
+
+/// Tries to display the User's global name and gets the username if they don't have one.
+pub fn username(user: &User) -> &str {
+    user.global_name.as_ref().unwrap_or(&user.name)
 }
 
 pub async fn get_users(
     user_ids: Vec<UserId>,
     cache_http: &impl CacheHttp,
 ) -> anyhow::Result<Vec<User>> {
-    try_join_all(user_ids.iter().map(|u| u.to_user(cache_http)))
+    future::try_join_all(user_ids.iter().map(|u| u.to_user(cache_http)))
         .await
         .context("Failed to fetch one or more users from the API")
 }
@@ -21,7 +38,7 @@ pub async fn get_guilds(
     guild_ids: &[GuildId],
     cache_http: &impl CacheHttp,
 ) -> anyhow::Result<Vec<PartialGuild>> {
-    try_join_all(guild_ids.iter().map(|g| {
+    future::try_join_all(guild_ids.iter().map(|g| {
         tracing::debug!("Fetching guild {g}");
         g.to_partial_guild(cache_http)
     }))
@@ -57,6 +74,14 @@ pub fn parse_role_ids(str: &str) -> anyhow::Result<Vec<RoleId>> {
             Err(e) => anyhow::bail!(e),
         })
         .collect()
+}
+
+pub fn display_bool(boolean: bool) -> &'static str {
+    if boolean {
+        "Yes"
+    } else {
+        "No"
+    }
 }
 
 #[cfg(test)]
